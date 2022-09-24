abcba714a48d4ebaaae76642281fbd1050ac2489
left0ver <1614674987@qq.com>
2022-09-25 02:04:16 +0800

    feat: 添加初始化数据库的script

---
+++ lib/initDataBase.ts
@@ -0,0 +1,27 @@
+import { DataSource } from 'typeorm'
+import { Question } from './entity/Question'
+import { databaseInfo, MyNamingStrategy } from './config'
+async function init() {
+  try {
+    const AppDataSource = new DataSource({
+      ...databaseInfo,
+      type: 'mysql',
+      entities: [Question],
+      synchronize: true,
+      logging: false,
+      namingStrategy: new MyNamingStrategy(),
+    })
+    await AppDataSource.initialize()
+    await AppDataSource.destroy()
+  } catch (error: any) {
+    throw new Error(error.message)
+  }
+}
+init()
+  .then(() => {
+    console.log('成功初始化表结构')
+  })
+  .catch(err => {
+    console.error(err)
+    console.error('初始化表结构失败')
+  })
